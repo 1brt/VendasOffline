35717a3258a92bf556f3994da3022a46433c04a1
lrgabriel <velhorg@hotmail.com>
2017-05-27 16:43:11 -0300

    Ajustes gerais.

--- app/src/main/java/br/com/vendasoffline/vendasoffline/activities/MainActivity.java
+++ app/src/main/java/br/com/vendasoffline/vendasoffline/activities/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.vendasoffline.vendasoffline.activities;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -76,11 +77,12 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            Intent intentRegister = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intentRegister);
+            finish();
             return true;
-        }*/
+        }
 
         return super.onOptionsItemSelected(item);
     }
