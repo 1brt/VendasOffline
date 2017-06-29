eb5b957dcf0ba02ef283aef1d59aa4723d23d577
1brt <hvcarvalhobrt@gmail.com>
2017-06-28 21:06:05 -0300

    Finalizado layout da tela de produto.
    Finalizado layout da tela de produto.s

--- app/src/main/java/br/com/vendasoffline/vendasoffline/classes/GetJson.java
+++ app/src/main/java/br/com/vendasoffline/vendasoffline/classes/GetJson.java
@@ -49,7 +49,7 @@ public class GetJson extends AsyncTask<Void, Void, Customer> {
         if (new NetworkChangeReceiver().isOnline(context)){
             Utils util = new Utils();
             //c = util.getInformacao("https://randomuser.me/api/");
-            c = util.getInformacao("http://web.effectiveerp.com.br:88/teste/ECommerce/ErpRestService.svc/ListarClientes/2");
+            c = util.getInformacao("http://web.effectiveerp.com.br:88/teste/ECommerce/ErpRestService.svc/AndroidListarClientes/2");
 
             (new DatabaseHelper(context)).addCustomer(c);
         }
